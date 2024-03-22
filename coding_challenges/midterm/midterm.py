297de09573a8d6623942707654f0aafd4a88f17b
madisonmilander <madisonmilander@gmail.com>
2024-03-22 11:33:21 -0400

    data added

--- coding_challenges/midterm/midterm.py
+++ coding_challenges/midterm/midterm.py
@@ -2,11 +2,35 @@
 # This first part is selecting the town of South Kingston
 
 import arcpy
-
 arcpy.env.overwriteOutput = True
 
+# This is the town selection of South Kingston, Rhode Island
 arcpy.analysis.Select(
-    in_features="staCons18",
-    out_feature_class=r"C:\Users\mmilander\OneDrive - University of Rhode Island\NRS 528\githubrepository\coding_challenges\midterm\midterm_528\study_area",
-    where_clause="NAME = 'Cioe/Tillinghast' Or NAME = 'Robert Pratt / 4400274.tif'"
+    in_features="towns.shp",
+    out_feature_class=r"C:\Users\mmilander\OneDrive - University of Rhode Island\NRS 528\githubrepository\coding_challenges\midterm\midterm_528\towns_studyarea",
+    where_clause="NAME = 'SOUTH KINGSTOWN'"
 )
+
+# Using the CLIP TOOl, the rivers and streams are narrowed down to the selected area (South Kingston)
+arcpy.analysis.Clip(
+    in_features="HYDRO_Rivers_and_Streams_24K.shp",
+    clip_features="towns_studyarea.shp",
+    out_feature_class=r"C:\Users\mmilander\OneDrive - University of Rhode Island\NRS 528\githubrepository\coding_challenges\midterm\midterm_528\HYDRO_Rivers_and_Stream_Clip2",
+    cluster_tolerance=None
+)
+
+# Once clipped to the area, I buffered the rivers by 100 meters of any space around it --> created a riparian zone
+arcpy.analysis.Buffer(
+    in_features="HYDRO_Rivers_and_Stream_Clip2.shp",
+    out_feature_class=r"C:\Users\mmilander\OneDrive - University of Rhode Island\NRS 528\githubrepository\coding_challenges\midterm\midterm_528\buffed_rivers",
+    buffer_distance_or_field="100 Meters",
+    line_side="FULL",
+    line_end_type="ROUND",
+    dissolve_option="ALL",
+    dissolve_field=None,
+    method="PLANAR"
+)
+
+print("Rivers are Buffered in South Kingston")
+
+
